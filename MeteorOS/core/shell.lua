045958e2d968b68b5f71b282a46bc46307040f27
gLiTcH <99803083+Mag1cpunch@users.noreply.github.com>
2025-06-30 14:30:09 +0300

    Fixed shell.lua, added while loop

--- MeteorOS/core/shell.lua
+++ MeteorOS/core/shell.lua
@@ -1,22 +1,29 @@
 local function main()
     term.clear()
     print("[ MeteorOS CLI Shell ]\n")
-    local input = read()
+
+    while true do
+        term.write("shell> ")
+        local input = read()
     
-    if input == "update" then
-        shell.run("wget", "https://raw.githubusercontent.com/Mag1cpunch/MeteorOS/refs/heads/main/setup.lua", "/setup.lua")
-        shell.run("/setup.lua")
-    elseif input == "reboot" then
-        print("Rebooting...")
-        os.reboot()
-    elseif input == "shutdown" then
-        print("Shutting down...")
-        os.shutdown()
-    elseif input == "help" then
-        print("Available commands:")
-        print(" - update: Update the system")
-        print(" - reboot: Reboot the system")
-        print(" - shutdown: Shut down the system")
+        if input == "update" then
+            shell.run("wget", "https://raw.githubusercontent.com/Mag1cpunch/MeteorOS/refs/heads/main/setup.lua", "/setup.lua")
+            shell.run("/setup.lua")
+        elseif input == "reboot" then
+            print("Rebooting...")
+            os.reboot()
+        elseif input == "shutdown" then
+            print("Shutting down...")
+            os.shutdown()
+        elseif input == "help" then
+            print("Available commands:")
+            print(" - update: Update the system")
+            print(" - reboot: Reboot the system")
+            print(" - shutdown: Shut down the system")
+        elseif input == "clear" then
+            term.clear()
+            print("[ MeteorOS CLI Shell ]\n")
+        end
     end
 end
 
