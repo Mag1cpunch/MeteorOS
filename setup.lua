6665d426e8cf5d982a72c612465f9fd712182afd
gLiTcH <99803083+Mag1cpunch@users.noreply.github.com>
2025-06-30 14:00:05 +0300

    Update setup.lua

--- setup.lua
+++ setup.lua
@@ -12,15 +12,11 @@ local manifest =
 }
 
 for _,v in ipairs(manifest) do
-    shell.execute("wget", v[1], v[2])
+    shell.run("wget", v[1], v[2])
 end
 
-for i = 5,5 do
-    print("Rebooting in "..i.." seconds")
-    sleep(1)
-    i = i - 1
-
-    if i <= 0 then
-        os.reboot()
-    end
+for i = 5, 1, -1 do
+    print("Rebooting in " .. i .. " seconds")
+    os.sleep(1)
 end
+os.reboot()
